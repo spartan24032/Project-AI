85cceee5f91562240e2670fcb54cf44e9bdc034c
Richard Denton <xylacon@gmail.com>
2024-04-05 14:14:37 -0500

    .

--- test.py
+++ test.py
@@ -6,8 +6,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-	# Gather data from various sources and assign them to variables
-	data_from_other = get_data()
-	
-    # Pass these variables to the html file
-	return render_template('index.html', data=data_from_other)
+	# Pass these variables to the html file
+	return render_template('index.html', data=get_data())
