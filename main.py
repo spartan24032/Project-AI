08707f22a1bb3c90acb87092f5aaa8a2c5240957
Richard Denton <xylacon@gmail.com>
2024-04-11 18:23:44 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -2,45 +2,41 @@
 from environment import GridWorld
 from agent import Agent
 from policies import PRandom, PExploit, PGreedy
-from runSimulation import run_simulation
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QCoreApplication, QThread
+import sys
+from runSimulation import SimulationWorker
 import random
 from UI import MockSimulationControl
 
-from UI import SimulationControl
-import sys
-from PyQt5.QtWidgets import QApplication
-
 if __name__ == "__main__":
-    app = QApplication(sys.argv)  # Create an instance of QApplication
-
     # Define the environment
     random.seed(5)
     dropoffCapacity = 5
     size = 5
     pickups = {  # {(coordinate): starting capacity}
-        (0,2): dropoffCapacity, (1,3): dropoffCapacity, (0,4): dropoffCapacity
+        (0,4): dropoffCapacity, (1,3): dropoffCapacity, (4,1): dropoffCapacity
                 }
     dropoffs = {  # {(coordinate): starting inventory}
-        (3,0): 0, (2,0): 0, (3,4): 0
+        (0,0): 0, (2,0): 0, (3,4): 0
     }
     
     env = GridWorld(size, pickups, dropoffs, dropoffCapacity)
 
     complex_world2 = True  # This uses a different state for each config of dropoff and pickup.
                             # It is great at finding the optimal policy, but uses 64x memory.
-    episode_based = True  # This chooses if we are using episodes or steps to run the simulation
+    episode_based = False  # This chooses if we are using episodes or steps to run the simulation
                            # False = steps, True = episodes
     #  "r" will either be episodes to run or total steps to run, depending on this value.
 
     a = env.actions
     agents = [
         # alpha = learning rate , gamma = discount factor
-        Agent(a, start_state=(0,0), policy=PExploit, learning_algorithm="Q-learning", alpha=0.7, gamma=0.8),
-        #Agent(a, start_state=(1,1), policy = PExploit, alpha=0.5, gamma=0.3),
-        #Agent(a, start_state=(4,4), policy=PExploit, alpha=0.3, gamma=0.5)
+        Agent(a, start_state=(2,2), policy = PExploit, learning_algorithm="Q-learning", alpha=0.5, gamma=0.3),
+        Agent(a, start_state=(4,2), policy=PExploit, learning_algorithm="Q-learning", alpha=0.3, gamma=0.5),
+        Agent(a, start_state=(0,2), policy=PExploit, learning_algorithm="Q-learning", alpha=0.7, gamma=0.8)
     ]
-    # sim_control = MockSimulationControl() # This is a mock control class that does nothing, allows code to run without UI
-    sim_control = SimulationControl() # This is a mock control class that does nothing, allows code to run without UI
-    sim_control.show()  # Show the main window
-    sys.exit(app.exec_())  # Start the event loop and wait for exit
-    run_simulation(agents, env, sim_control, complex_world2, episode_based, r=300)
+    r = 100
+    sim_control = MockSimulationControl() # This is a mock control class that does nothing, allows code to run without UI
+    simulationWorker = SimulationWorker(agents, env, complex_world2, episode_based, r, mskip = True)
+    simulationWorker.run_simulation()
