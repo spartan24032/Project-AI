106103aa9c002370684e87847ee289d0ea3fd970
Anh Vu <95508707+anhvu0@users.noreply.github.com>
2024-04-12 06:11:59 -0500

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,83 @@
+# agent.py
+class Agent:
+    def __init__(self, actions, start_state, policy, learning_algorithm="Q-learning", alpha=0.5, gamma=0.5,
+                 override_policy=None, override_max_step=0):
+        self.Q_dicts = {}
+        self.learning_algorithm = learning_algorithm
+        self.alpha = alpha
+        self.gamma = gamma
+        self.actions = actions  # Possible actions
+        self.state = start_state  # Agent's current state (position, has_item)
+        self.reset_state = start_state
+
+        self.default_policy = policy
+        self.policy = policy  # Current policy in use
+        self.override_policy = override_policy
+        self.terminate_override_step = override_max_step
+
+    def reset(self, pd_string):
+        self.state = self.reset_state
+        self.has_item = False
+        if pd_string not in self.Q_dicts:
+            self.Q_dicts[pd_string] = {}
+    
+    def get_state(self):
+        return self.state, self.has_item
+    
+    def update_state(self, new_state, has_item):
+        self.state = new_state
+        self.has_item = has_item
+
+    def update_q_values(self, state, has_item, action, valid_actions, reward, next_state, next_has_item, pd_string, next_action):
+        if pd_string not in self.Q_dicts:
+            self.Q_dicts[pd_string] = {}
+
+        if self.learning_algorithm == "Q-learning":
+            current_q = self.Q_dicts[pd_string].get((state, has_item, action), 0)
+            # Find the max Q-value for the NEXT state across all FUTURE possible actions
+            q_values_next = [
+                self.Q_dicts[pd_string].get((next_state, next_has_item, a))
+                for a in valid_actions if (next_state, next_has_item, a) in self.Q_dicts[pd_string]
+            ]
+            next_max_q = max(q_values_next) if q_values_next else 0
+            new_q = (current_q * (1 - self.alpha)) + self.alpha * (reward + self.gamma * next_max_q)
+
+            self.Q_dicts[pd_string][(state, has_item, action)] = new_q
+
+        if self.learning_algorithm == "SARSA":
+            current_q = self.Q_dicts[pd_string].get((state, has_item, action), 0)
+            # In SARSA, the action A' is chosen using the current policy from S' (it's passed into the function)
+            next_q = self.Q_dicts[pd_string].get((next_state, next_has_item, next_action), 0)
+            new_q = current_q + self.alpha * (reward + self.gamma * next_q - current_q)
+            self.Q_dicts[pd_string][(state, has_item, action)] = new_q
+
+    def choose_action(self, valid_actions, pd_string, step, episode, ep_based):
+        if pd_string not in self.Q_dicts:
+            self.Q_dicts[pd_string] = {}
+        action = self.policy(self.state, self.has_item, self.Q_dicts[pd_string], valid_actions)
+        if self.terminate_override_step != 0:
+            if not ep_based:
+                if self.override_policy and self.terminate_override_step > step:
+                    action = self.override_policy(self.state, self.has_item, self.Q_dicts[pd_string], valid_actions)
+                    if self.terminate_override_step <= step: # Revert to default policy if override duration has elapsed
+                        self.policy = self.default_policy
+            elif ep_based:
+                if self.override_policy and self.terminate_override_step > episode:
+                    action = self.override_policy(self.state, self.has_item, self.Q_dicts[pd_string], valid_actions)
+                    if self.terminate_override_step <= episode:
+                        self.policy = self.default_policy
+        return action
+
+    def return_q_dicts(self):
+        return self.Q_dicts
+
+    def display_q_values(self, pd_string):
+        if pd_string == '5':
+            print(f"  Q-values:")
+        else:
+            print(f"  Q-values (using complex state2, P/D states: {pd_string})")
+        for action in self.actions:
+            q_value = self.Q_dicts[pd_string].get((self.state, self.has_item, action))
+            display_value = "--" if q_value is None else f"{q_value:.2f}"
+            print(f"    {action}: {display_value}")
+
---
+++ runSimulation.py
@@ -0,0 +1,145 @@
+# runSimulation.py
+import time
+from PyQt5.QtCore import pyqtSignal, QObject, QThread, QTimer
+import copy
+
+class SimulationWorker(QObject):
+    finished = pyqtSignal()
+    # self.agents, env, episode, step, self.r
+    update_display = pyqtSignal(object, object, int, int, int, bool)
+    # idx, agent_buffer, valid_actions_current, pd_string, action, reward
+    update_qtable_display = pyqtSignal(int, object, list, str, str, int)
+    requestPause = pyqtSignal()
+    requestPlay = pyqtSignal(int)
+    requestNext = pyqtSignal()
+    requestSkip = pyqtSignal(int)
+
+    def __init__(self, agents, env, complex_world2, episode_based, r, mskip):
+        super().__init__()
+        self.agents = agents
+        self.env = env
+        self.complex_world2 = complex_world2
+        self.episode_based = episode_based
+        self.r = r
+        self.paused = True
+        self.moveOne = 0
+        self.skipTo = None
+        self.autoPlay = False
+        self.autoPlay_speed = 1
+        self.requestPause.connect(self.onPause)
+        self.requestPlay.connect(self.onPlay)
+        self.requestNext.connect(self.onNext)
+        self.requestSkip.connect(self.onSkip)
+        self.mskip = mskip
+        self.endReached = False
+
+    def onPause(self):
+        self.paused = True
+
+    def onPlay(self, speed):
+        self.paused = False
+        self.autoPlay = True
+        self.autoPlay_speed = speed
+
+    def onNext(self):
+        self.moveOne = self.moveOne + 1
+
+    def onSkip(self, targetN):
+        self.skipTo = targetN
+        self.paused = True
+
+    def run_simulation(self):
+
+
+        if self.episode_based:
+            outer_loop = self.r
+        else:
+            outer_loop = 1
+
+        for episode in range(outer_loop):
+            self.env.reset()
+            pd_string = self.env.generate_pd_string(self.complex_world2)
+            for agent in self.agents:
+                agent.reset(pd_string)
+
+            # use verbose to control which episodes get output
+            # [0, episodes - 1] to see the first and last.
+            verbose = episode in [outer_loop - 1]
+
+            if verbose:
+                print(f"--- Episode {episode + 1} ---")
+            if not self.episode_based:
+                print(f"--- Running One Simulation with {self.r} total steps --- ")
+            total_reward = 0
+            step = 0
+            while( (self.episode_based == False and step < self.r) or (self.episode_based == True and not self.env.dropoffs_complete()) ):
+                if not self.episode_based:
+                    verbose = step % 60 == 0
+
+                if verbose:
+                    print(f"\nStep {step}")
+                    self.env.render(self.agents)
+                pd_string = self.env.generate_pd_string(self.complex_world2)
+                if self.skipTo is None:
+                    agent_buffer = copy.deepcopy(self.agents)
+                    environment_buffer = copy.deepcopy(self.env)
+                    pd_buffer = copy.deepcopy(pd_string)
+                    self.update_display.emit(agent_buffer, environment_buffer, episode, step, self.r, self.episode_based)
+                actions_taken = []
+
+                for idx, agent in enumerate(self.agents):
+                    if self.env.dropoffs_complete() and not self.endReached:
+                        self.paused = True
+                        self.autoPlay = False
+                        self.skipTo = None
+                        self.endReached = True
+                        if verbose:
+                            print(f"\nStep {step + 1}")
+                            self.env.render(self.agents)
+                            print(f"All dropoffs complete.\nTotal Reward for Episode {episode + 1}: {total_reward}\n")
+                    if (not self.episode_based and step > self.r):
+                        print("reached max steps OR completed all dropoffs, killing program")
+                        exit()
+                    old_state, old_has_item = agent.get_state()
+                    # Get valid actions for the CURRENT state, before action is chosen
+                    valid_actions_current = self.env.valid_actions(agent.get_state(), self.agents)
+                    action = agent.choose_action(valid_actions_current, pd_string, step, episode, self.episode_based)
+                    if verbose:
+                        print(f"\033[91mAgent {idx}\033[0m {old_state}, Valid Actions: {valid_actions_current}")
+                        self.agents[idx].display_q_values(pd_string)
+                    reward = self.env.step(agent, action)  # Perform the action, moving to the new state
+                    step += 1
+                    # print(f"step {step}")
+                    total_reward += reward
+                    if verbose:
+                        print(f"  selecting: {action}, Reward: {reward}")
+                    if self.skipTo is None:
+                        self.update_qtable_display.emit(idx, agent_buffer, valid_actions_current, pd_buffer, action, reward)
+                    # Now, get valid actions for the NEW state, after action is performed
+                    new_state, new_has_item = agent.get_state()  # This is effectively 'next_state' for Q-value update
+                    valid_actions_next = self.env.valid_actions(agent.get_state(), self.agents)
+                    # next_action is only for SARSA as it needs the future action based on policy
+                    next_action = agent.choose_action(valid_actions_next, pd_string, step, episode, self.episode_based)
+                    # Update Q-values using 'old_state' as current and 'new_state' as next
+                    agent.update_q_values(old_state, old_has_item, action, valid_actions_next, reward, new_state,
+                                          new_has_item, pd_string, next_action)
+
+                    actions_taken.append((idx, action, reward, new_state, valid_actions_current))
+
+                if not self.mskip:
+                    if self.skipTo is not None:
+                        if self.episode_based and episode > (int(self.skipTo) -1):
+                            self.skipTo = None  # Reset skipping logic
+                        elif not self.episode_based and step > (int(self.skipTo) - 1):
+                            self.skipTo = None  # Reset skipping logic
+                        continue
+                    if self.paused:
+                        while self.paused:
+                            time.sleep(0.1)
+                            if(self.moveOne > 0):
+                                self.moveOne = self.moveOne - 1
+                                break
+                    if self.autoPlay:
+                        time.sleep((101- (self.autoPlay_speed+30)*2) / 100)
+
+        self.finished.emit()
