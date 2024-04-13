eb21a7f5e07b556fdd44b8f5471e99da4a9c1fd1
Richard Denton <xylacon@gmail.com>
2024-04-12 19:47:50 -0500

    .

--- runSimulation.py
+++ runSimulation.py
@@ -1,90 +1,142 @@
 # runSimulation.py
 import time
+from PyQt5.QtCore import pyqtSignal, QObject, QThread, QTimer
+import copy
 
-def run_simulation(agents, env, sim_control, complex_world2=False, episode_based=True, r=200):
+class SimulationWorker(QObject):
+    finished = pyqtSignal()
+    update_display = pyqtSignal(object, object, int, int, int) # self.agents, env, episode, step, self.r
+    update_qtable_display = pyqtSignal(object) # self.agents
+    requestPause = pyqtSignal()
+    requestPlay = pyqtSignal(int)
+    requestNext = pyqtSignal()
+    requestSkip = pyqtSignal(int)
 
-    if episode_based:
-        outer_loop = r
-    else:
-        outer_loop = 1
+    def __init__(self, agents, env, complex_world2, episode_based, r, mskip):
+        super().__init__()
+        self.agents = agents
+        self.env = env
+        self.complex_world2 = complex_world2
+        self.episode_based = episode_based
+        self.r = r
+        self.paused = True
+        self.moveOne = 0
+        self.skipTo = None
+        self.autoPlay = False
+        self.autoPlay_speed = 1
+        self.requestPause.connect(self.onPause)
+        self.requestPlay.connect(self.onPlay)
+        self.requestNext.connect(self.onNext)
+        self.requestSkip.connect(self.onSkip)
+        self.mskip = mskip
 
-    for episode in range(outer_loop):
-        env.reset()
-        pd_string = env.generate_pd_string(complex_world2)
-        for agent in agents:
-            agent.reset(pd_string)
+    def onPause(self):
+        self.paused = True
 
-        # use verbose to control which episodes get output
-        # [0, episodes - 1] to see the first and last.
-        verbose = episode in [r - 1]
+    def onPlay(self, speed):
+        self.paused = False
+        self.autoPlay = True
+        self.autoPlay_speed = speed
 
-        if verbose:
-            print(f"--- Episode {episode + 1} ---")
-        if not episode_based:
-            print(f"--- Running One Simulation with {r} total steps --- ")
-        total_reward = 0
-        step = -1
-        while( (episode_based == False and step < r) or (episode_based == True and not env.dropoffs_complete()) ):
-            #if not episode_based:
-            #   verbose = step % 10 == 0
-            verbose = step % 1 == 0
-            step += 1
-            if sim_control.skip_to_step is None:
-                sim_control.updateCurrentWorldDisplay(agents, env, episode, step, r)
+    def onNext(self):
+        self.moveOne = self.moveOne + 1
+
+    def onSkip(self, targetN):
+        self.skipTo = targetN
+        self.paused = True
+
+    def run_simulation(self):
+
+
+        if self.episode_based:
+            outer_loop = self.r
+        else:
+            outer_loop = 1
+
+        for episode in range(outer_loop):
+            self.env.reset()
+            pd_string = self.env.generate_pd_string(self.complex_world2)
+            for agent in self.agents:
+                agent.reset(pd_string)
+
+            # use verbose to control which episodes get output
+            # [0, episodes - 1] to see the first and last.
+            verbose = episode in [outer_loop - 1]
+
             if verbose:
-                print(f"\nStep {step}")
-                env.render(agents)
+                print(f"--- Episode {episode + 1} ---")
+            if not self.episode_based:
+                print(f"--- Running One Simulation with {self.r} total steps --- ")
+            total_reward = 0
+            step = -1
+            while( (self.episode_based == False and step < self.r) or (self.episode_based == True and not self.env.dropoffs_complete()) ):
+                #if not episode_based:
+                #   verbose = step % 10 == 0
+                # verbose = step % 1 == 0
+                step += 1
 
-            actions_taken = []
-            for idx, agent in enumerate(agents):
-                if env.dropoffs_complete():
+                if verbose:
+                    print(f"\nStep {step}")
+                    self.env.render(self.agents)
+                if self.skipTo is None:
+                    agent_buffer = copy.deepcopy(self.agents)
+                    environment_buffer = copy.deepcopy(self.env)
+                    self.update_display.emit(agent_buffer, environment_buffer, episode, step, self.r)
+                actions_taken = []
+                for idx, agent in enumerate(self.agents):
+                    if self.env.dropoffs_complete():
+                        if verbose:
+                            print(f"\nStep {step + 1}")
+                            self.env.render(self.agents)
+                            print(f"All dropoffs complete.\nTotal Reward for Episode {episode + 1}: {total_reward}\n")
+                        break
+                    if not self.episode_based and step > self.r or self.env.dropoffs_complete():
+                        print("reached max steps OR completed all dropoffs, killing program")
+                        exit()
+                    old_state, old_has_item = agent.get_state()
+                    # Get valid actions for the CURRENT state, before action is chosen
+                    valid_actions_current = self.env.valid_actions(agent.get_state(), self.agents)
+                    action = agent.choose_action(valid_actions_current, pd_string)
                     if verbose:
-                        print(f"\nStep {step + 1}")
-                        env.render(agents)
-                        print(f"All dropoffs complete.\nTotal Reward for Episode {episode + 1}: {total_reward}\n")
-                    break
-                if not episode_based and step > r or env.dropoffs_complete():
-                    print("reached max steps OR completed all dropoffs, killing program")
-                    exit()
-                old_state, old_has_item = agent.get_state()
-                # Get valid actions for the CURRENT state, before action is chosen
-                valid_actions_current = env.valid_actions(agent.get_state(), agents)
-                action = agent.choose_action(valid_actions_current, pd_string)
-                if verbose:
-                    print(f"\033[91mAgent {idx}\033[0m {old_state}, Valid Actions: {valid_actions_current}")
-                    agents[idx].display_q_values(pd_string)
-                reward = env.step(agent, action)  # Perform the action, moving to the new state
-                total_reward += reward
-                pd_string = env.generate_pd_string(complex_world2)
-                if verbose:
-                    print(f"  selecting: {action}, Reward: {reward}")
+                        print(f"\033[91mAgent {idx}\033[0m {old_state}, Valid Actions: {valid_actions_current}")
+                        self.agents[idx].display_q_values(pd_string)
 
-                # Now, get valid actions for the NEW state, after action is performed
-                new_state, new_has_item = agent.get_state()  # This is effectively 'next_state' for Q-value update
-                valid_actions_next = env.valid_actions(agent.get_state(), agents)
-                # next_action is only for SARSA as it needs the future action based on policy
-                next_action = agent.choose_action(valid_actions_next, pd_string)
-                # Update Q-values using 'old_state' as current and 'new_state' as next
-                agent.update_q_values(old_state, old_has_item, action, valid_actions_next, reward, new_state,
-                                      new_has_item, pd_string, next_action)
+                    reward = self.env.step(agent, action)  # Perform the action, moving to the new state
+                    total_reward += reward
+                    pd_string = self.env.generate_pd_string(self.complex_world2)
+                    if verbose:
+                        print(f"  selecting: {action}, Reward: {reward}")
+                    # Now, get valid actions for the NEW state, after action is performed
+                    new_state, new_has_item = agent.get_state()  # This is effectively 'next_state' for Q-value update
+                    valid_actions_next = self.env.valid_actions(agent.get_state(), self.agents)
+                    # next_action is only for SARSA as it needs the future action based on policy
+                    next_action = agent.choose_action(valid_actions_next, pd_string)
+                    # Update Q-values using 'old_state' as current and 'new_state' as next
+                    agent.update_q_values(old_state, old_has_item, action, valid_actions_next, reward, new_state,
+                                          new_has_item, pd_string, next_action)
 
-                actions_taken.append((idx, action, reward, new_state, valid_actions_current))
+                    actions_taken.append((idx, action, reward, new_state, valid_actions_current))
 
-            if not sim_control.masterskip:
-                # Skip functionality
-                if sim_control.skip_to_step is not None:
-                    print(f"skip to step/episode enabled, skipping to {sim_control.skip_to_step}")
-                    if episode_based and episode >= (int(sim_control.skip_to_step)):
-                        sim_control.skip_to_step = None  # Reset skipping logic
-                        sim_control.updateCurrentWorldDisplay(agents, env, episode, step, r)
-                    elif not episode_based and step >= (int(sim_control.skip_to_step)-1):
-                        sim_control.skip_to_step = None  # Reset skipping logic
-                        sim_control.updateCurrentWorldDisplay(agents, env, episode, step, r)
-                    continue  # Proceed to the next iteration without executing further actions
-                if sim_control.autoplay_enabled:
-                    print("autoplay enabled")
-                    time.sleep(5.0 / sim_control.speedSlider.value())  # Adjust based on the speed slider
-                else:
-                    print("waiting on next step signal")
-                    sim_control.next_step_event.wait()  # Wait for the next step signal
-                    sim_control.next_step_event.clear()  # Reset the event for the next iteration
+                if self.skipTo is None:
+                    agents_buffer = copy.deepcopy(self.agents)
+                    self.update_qtable_display.emit(agents_buffer)
+
+                if not self.mskip:
+                    if self.skipTo is not None:
+                        if self.episode_based and episode > (int(self.skipTo) -1):
+                            self.skipTo = None  # Reset skipping logic
+                            # self.sim_control.updateCurrentWorldDisplay(self.agents, env, episode, step, self.r)
+                        elif not self.episode_based and step > (int(self.skipTo) - 1):
+                            self.skipTo = None  # Reset skipping logic
+                            # self.sim_control.updateCurrentWorldDisplay(self.agents, env, episode, step, self.r)
+                        continue
+                    if self.paused:
+                        while self.paused:
+                            time.sleep(0.1)
+                            if(self.moveOne > 0):
+                                self.moveOne = self.moveOne - 1
+                                break
+                    if self.autoPlay:
+                        time.sleep((101- (self.autoPlay_speed+30)*2) / 100)
+
+        self.finished.emit()
